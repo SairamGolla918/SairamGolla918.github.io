fc979192afe5291484981d0ec17259c465421ad6
SairamGolla918 <gollasairam918@gmail.com>
2024-11-03 20:57:45 +0530

    added light mode & dark mode functoinality

--- js/script.js
+++ js/script.js
@@ -4,3 +4,30 @@
  */
 
 "use strict";
+
+// light and dark mode
+
+// Node Element
+const $themeBtn = document.querySelector("[data-theme-btn]");
+const $HTML = document.documentElement;
+
+// Boolean | String
+
+let isDark = window.matchMedia("(prefer-color-scheme:dark)").matches;
+
+if(sessionStorage.getItem("theme")){
+    $HTML.dataset.theme = sessionStorage.getItem("theme")
+}
+else{
+    $HTML.dataset.theme=isDark ? "dark" : "light";
+    sessionStorage.setItem("theme",$HTML.dataset.theme);
+}
+
+const changeTheme = () => {
+
+    $HTML.dataset.theme = sessionStorage.getItem("theme") === "light" ? "dark" :"light";
+
+    sessionStorage.setItem("theme",$HTML.dataset.theme);
+}
+
+$themeBtn.addEventListener("click",changeTheme);
